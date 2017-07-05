c4a49ec0583d63ed0d7f4acecb6759a6196ddc68
Nicholas Burley <deusvultdev@gmail.com>
2017-07-05 00:26:01 -0500

    Update ck2_populate_songs.py

--- ck2_populate_songs.py
+++ ck2_populate_songs.py
@@ -1,9 +1,19 @@
 import os
+import re
+import fnmatch
 
 outfile = open("songs.txt", "w")
 
-indir = '.'
-for root, dirs, filenames in os.walk(indir):
+#Setup ogg inclusion
+includes = ['*.ogg']
+includes = r'|'.join([fnmatch.translate(x) for x in includes])
+
+#Step through the directory
+for root, dirs, filenames in os.walk('.'):
+	#Filter out everything but ogg files
+    filenames = [f for f in filenames if re.match(includes, f)]
+
+    #Write all entries
     for f in filenames:
         outfile.write("song = {\n")
         outfile.write("\tname = \"" + f + "\"\n")
@@ -12,3 +22,5 @@ for root, dirs, filenames in os.walk(indir):
         outfile.write("\t\tfactor = 1\n")
         outfile.write("\t}\n")
         outfile.write("}\n\n")
+
+outfile.close()
